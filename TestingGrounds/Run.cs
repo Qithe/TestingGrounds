0c299a69cd7b5ef2bc0e7145551482c3b8d47812
Oscar Wolter <Oscardawolter@gmail.com>
2017-10-10 14:52:01 +0200

    Add project files.

---
+++ TestingGrounds/Run.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGrounds
+{
+    class Run
+    {
+        public static int indexCounter = 0;
+        public static Random rnd = new Random();
+        public static void RunSim()
+        {
+            List<Mob> MobList = InitialCreaton();
+            
+            do
+            {
+                Printer(MobList);
+                BattlePhase(MobList);
+                BreedingPhase(MobList);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            
+            
+            
+        }
+        //Creates the "prime" creatures, the first iteration, generation Zero
+        private static List<Mob> InitialCreaton()
+        {
+            List<Mob> list = new List<Mob>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(new Mob("Wolf", i,50,15,15,15,"Prime",5));
+                indexCounter++;
+            }
+            return (list); 
+        }
+        //puts all creatures agains eachother, the quickest attacks first, targeting the weak first.
+        //Each kill grants the killer with 1/3 its attack in health.
+
+        //Rules: if pray agility is twice as mutch as killer agility, pray takes no damage,
+        //and has a sligh chance of hitting killer with full attack, ignoring agility.
+        //has pray half of killers agility or more, same agility or more, 150% or more(up to 200%), 
+        //pray avoids 25%,50%,75% of killer attack( in that order)
+        private static List<Mob> BattlePhase(List<Mob> list)
+        {
+            var speedList = from sl in list
+                            orderby sl.Speed descending
+                            select sl;
+            var healthList = from hl in list
+                             orderby hl.Health ascending
+                             select hl;
+
+            //Mob(i) checks for pray
+            for (int i = 0; i < speedList.Count(); i++)
+            {
+                //checks if killer is dead.
+                if (speedList.ElementAt(i).Health > 0)
+                {
+                    //loops thru pray(j)
+                    for (int j = 0; j < healthList.Count(); j++)
+                    {
+                        //mob(i) checks if mob(j) is alive and is not itself
+                        if (healthList.ElementAt(j).Health > 0 && speedList.ElementAt(i).Id != healthList.ElementAt(j).Id)
+                        {
+                            healthList.ElementAt(j).Health -= speedList.ElementAt(i).Attack;
+                            if (healthList.ElementAt(j).Health < 0)
+                            {
+                                speedList.ElementAt(i).Health += speedList.ElementAt(i).Attack / 3;
+                            }
+
+                        }
+                    }
+                }
+                 
+            }
+            //makes a new list with all the dead mobs removed
+            var results = from res in speedList
+                          orderby res.Id ascending
+                          select res;
+            for (int i = 0; i < list.Count; i++)
+            {
+                list.ElementAt(i).Health = speedList.ElementAt(i).Health;
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list.ElementAt(i).Health <= 0)
+                {
+                    list.Remove(list.ElementAt(i));
+                }
+            }
+            return (list);
+            
+        }
+        //breeds the creatures to the new generation, This is totaly random and 
+        //if they breed and how many they breed is determend by breedability.
+        private static void BreedingPhase(List<Mob> list)
+        {
+            List<int> hasbreed = new List<int>();
+            List<int> hasNotBreed = new List<int>();
+            int breedCykles = list.Count / 2;
+            int firstMate;
+            int secondMate;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                hasNotBreed.Add(list.ElementAt(i).Id);
+            }
+
+            for (int i = 0; i < breedCykles; i++)
+            {
+                firstMate = rnd.Next(hasNotBreed.Count);
+                hasbreed.Add(firstMate);
+                hasNotBreed.Remove(firstMate);
+                secondMate = rnd.Next(hasNotBreed.Count);
+                hasbreed.Add(secondMate);
+                hasNotBreed.Remove(secondMate);
+
+                var firstMateStats = from fms in list
+                                     where fms.Id == firstMate
+                                     select fms;
+                var secondMateStats = from sms in list
+                                      where sms.Id == secondMate
+                                      select sms;
+
+                int tempHealth = (firstMateStats.First().Health + secondMateStats.First().Health) / 2;
+                int tempAttack = (firstMateStats.First().Attack + secondMateStats.First().Attack) / 2;
+                int tempSpeed = (firstMateStats.First().Speed + secondMateStats.First().Speed) / 2;
+                int tempAgility = (firstMateStats.First().Agility + secondMateStats.First().Agility) / 2;
+                int tempBreedablilty = (firstMateStats.First().Breedability + secondMateStats.First().Breedability) / 2;
+                string tempParents = firstMateStats.First().Id.ToString("000") + secondMateStats.First().Id.ToString("000");
+
+                list.Add(new Mob("Wolf", indexCounter, tempHealth, tempAttack, tempSpeed, tempAgility, tempParents, tempBreedablilty));
+            }
+
+
+        }
+
+        
+
+        private static void Printer(List<Mob> list)
+        {
+            Console.Clear();
+            Console.WriteLine("TYPE,    ID, HEALTH, ATTACK, SPEED, AGILITY, KILLSTREAK, Breed, Parents");
+            foreach (Mob listItem in list)
+            {
+                Console.WriteLine(
+                    $"{listItem.Type}, " +
+                    $"{listItem.Id.ToString("00000")}, " +
+                    $"{listItem.Health.ToString("000000")}, " +
+                    $"{listItem.Attack.ToString("000000")}, " +
+                    $"{listItem.Speed.ToString("00000")}, " +
+                    $"{listItem.Agility.ToString("0000000")}, " +
+                    $"{listItem.KillStreak.ToString("0000000000")}, " +
+                    $"{listItem.Breedability.ToString("00000")}, " +
+                    $"{listItem.Parents} "
+                    );
+            }
+        }
+    }
+}
---
+++ TestingGrounds/WorldObjects.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGrounds
+{
+    public class Mob
+    {
+        public int Health { get; set; }
+        public int Agility { get; set; }
+        public int Speed { get; set; }
+        public int Attack { get; set; }
+        public string Type { get; set; }
+        public int Id { get; set; }
+        public int KillStreak { get; set; }
+        public int Breedability { get; set; } 
+        public string Parents { get; set; }
+
+        public static Random rnd = new Random();
+        
+
+        public Mob( string type, int id, int health, int attack, int speed, int agility, string parents, int breedability)
+        {
+            this.Type = type;
+            this.Id = id;
+            this.Health = SetHealth(health);
+            this.Speed = SetSpeed(speed);
+            this.Agility = SetAgility(agility);
+            this.Attack = SetAttack(attack, agility);
+            this.Parents = parents;
+            this.Breedability = SetBreedability(breedability);
+        }
+        public static int SetHealth(int health)
+        {
+            return (health += Mob.rnd.Next(-2, 2));
+        }
+        public static int SetAttack(int attack, int agility)
+        {
+            return (attack += Mob.rnd.Next(-2, 2) + agility);
+        }
+        public static int SetSpeed(int speed)
+        {
+            return (speed += Mob.rnd.Next(-2, 2));
+        }
+        public static int SetAgility(int agility)
+        {
+            return (agility += Mob.rnd.Next(-2, 2));
+        }
+        public static int SetBreedability(int breedability)
+        {
+            return (breedability += Mob.rnd.Next(-1, 1));
+        }
+
+
+    }
+
+    
+}
